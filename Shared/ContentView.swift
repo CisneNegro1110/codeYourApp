6029b7e1c65cc0d7327cd9167a86119dd39fd0ff
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-03-21 16:52:42 -0700

    formulario

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name: String = ""
+    @State private var age = Int.self
+    @State private var email: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            VStack {
+                TextField("Name", text: $name)
+                TextField("Age", value: $age, formatter: NumberFormatter())
+                    .keyboardType(UIKeyboardType.decimalPad)
+                TextField("email", text: $email)
+            }.padding()
+            Spacer()
+        }
     }
 }
 
