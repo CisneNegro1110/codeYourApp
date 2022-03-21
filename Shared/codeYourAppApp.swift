2e082183d94b879e222d3ca01ea1bd90bde2dc3d
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-03-21 07:04:50 -0700

    Initial Commit

---
+++ Shared/codeYourAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  codeYourAppApp.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 21/03/22.
+//
+
+import SwiftUI
+
+@main
+struct codeYourAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
